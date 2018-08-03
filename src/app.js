1ec404f57cdc0378abe2c9c9479144116ea8bb70
Darron Brown <DarronBrown88@outlook.com>
2018-08-02 20:46:06 -0600

    react stateful reloading

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Counter from './counter';
+import { AppContainer } from 'react-hot-loader';
 
-const App = () => (
-	<div>
-		<Counter />
-	</div>
-);
+function render(Component) {
+	ReactDom.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById("react-root")
+	);
+}
 
-ReactDom.render(<App />,document.getElementById("react-root"));
+render(Counter);
+
+if (module.hot) {
+	module.hot.accept("./counter.js"), () => {
+		const NewCounter = require("./counter.js").default;
+		render(NewCounter)
+	}
+}
