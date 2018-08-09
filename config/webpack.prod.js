d3feaa4722cf0e583cc67feb8f3fd91501655c26
Darron Brown <DarronBrown88@outlook.com>
2018-08-09 09:23:53 -0600

    compressed css in dist folder

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,8 @@
-const path             = require("path");
-const webpack          = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const path                    = require("path");
+const webpack                 = require("webpack");
+const HTMLWebpackPlugin       = require("html-webpack-plugin");
+const MiniCSSExtractPlugin    = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
@@ -112,7 +113,10 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new MiniCSSExtractPlugin(),
+		new OptimizeCssAssetsPlugin(),
+		new MiniCSSExtractPlugin({
+			filename: "[name]-[contenthash].css"
+		}),
 		new HTMLWebpackPlugin({
 			template: "./src/index.html"
 		})
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // require("babel-runtime/regenerator");
 // require("react-hot-loader/patch");
 require("./main.css");
+require("./nav.css");
 require("./index.html");
 require("./app");
